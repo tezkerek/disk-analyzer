cc5940e5bf388366cc95142be9f839d403a52271
Tezkerek <andrei.ancuta@gmail.com>
2022-12-26 21:26:16 +0200

    Minor changes
    Add --help flag
    Handle missing command
    Change print shortopt to -p
    Check priority value
    Put the absolute path in the arg struct

--- src/client/arg_parse.c
+++ src/client/arg_parse.c
@@ -1,113 +1,121 @@
-#include <getopt.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <client/arg_parse.h>
 #include <common/ipc.h>
 #include <common/utils.h>
+#include <getopt.h>
+#include <linux/limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int get_args(int argc, char **argv, struct da_args *ret) {
-    int c;
-    ret->cmd = -1;
+#define DEFAULT_PRIORITY 2
+
+static struct option long_options[] = {
+    {"add",      required_argument, 0, 'a'},
+    {"priority", required_argument, 0, 'p'},
+    {"suspend",  required_argument, 0, 'S'},
+    {"resume",   required_argument, 0, 'R'},
+    {"remove",   required_argument, 0, 'r'},
+    {"info",     required_argument, 0, 'i'},
+    {"list",     no_argument,       0, 'l'},
+    {"print",    required_argument, 0, 'P'},
+    {"help",     no_argument,       0, 'h'},
+    {0,          0,                 0, 0  }
+};
+
+int parse_args(int argc, char **argv, struct da_args *result) {
+    result->cmd = -1;
+    result->priority = DEFAULT_PRIORITY;
 
     while (1) {
-        int option_index = 0;
-        static struct option long_options[] = {
-            {"add",      required_argument, 0, 'a'},
-            {"priority", required_argument, 0, 'p'},
-            {"suspend",  required_argument, 0, 'S'},
-            {"resume",   required_argument, 0, 'R'},
-            {"remove",   required_argument, 0, 'r'},
-            {"info",     required_argument, 0, 'i'},
-            {"list",     no_argument,       0, 'l'},
-            {"print",    required_argument, 0, 'p'},
-            {0,          0,                 0, 0  }
-        };
-
-        c = getopt_long(argc, argv, "a:p:S:R:r:i:lp:", long_options, &option_index);
+        int c = getopt_long(argc, argv, "a:p:S:R:r:i:lP:h", long_options, NULL);
         if (c == -1)
             break;
 
         switch (c) {
-
         case 'a':
-            if (ret->cmd != -1) {
+            if (result->cmd != -1) {
                 return -1;
             }
-            ret->cmd = CMD_ADD;
-            int const path_len = strlen(optarg);
-            ret->path = da_malloc(path_len + 1);
-            strncpy(ret->path, optarg, path_len);
-            ret->priority = DEFAULT_PRIORITY;
+            result->cmd = CMD_ADD;
+            result->path = da_malloc(PATH_MAX);
+            realpath(optarg, result->path);
             break;
 
         case 'p':
-            if (ret->cmd == CMD_ADD) {
-                ret->priority = atoi(optarg);
-            } else if (ret->cmd == -1) {
-                ret->cmd = CMD_PRINT;
-                ret->job_id = atoll(optarg);
-            } else {
-                printf("%s", help);
+            int8_t priority = atoi(optarg);
+            if (priority < 1 || priority > 3) {
+                fputs("Priority must be between 1 and 3\n", stderr);
                 return -1;
             }
+            result->priority = priority;
+            break;
+
+        case 'P':
+            if (result->cmd != -1) {
+                return -1;
+            }
+            result->cmd = CMD_PRINT;
+            result->job_id = atoll(optarg);
             break;
 
         case 'S':
-            if (ret->cmd != -1) {
-                printf("%s", help);
+            if (result->cmd != -1) {
                 return -1;
             }
-            ret->cmd = CMD_SUSPEND;
-            ret->job_id = atoll(optarg);
+            result->cmd = CMD_SUSPEND;
+            result->job_id = atoll(optarg);
             break;
 
         case 'R':
-            if (ret->cmd != -1) {
-                printf("%s", help);
+            if (result->cmd != -1) {
                 return -1;
             }
-            ret->cmd = CMD_RESUME;
-            ret->job_id = atoll(optarg);
+            result->cmd = CMD_RESUME;
+            result->job_id = atoll(optarg);
             break;
 
         case 'r':
-            if (ret->cmd != -1) {
-                printf("%s", help);
+            if (result->cmd != -1) {
                 return -1;
             }
-            ret->cmd = CMD_REMOVE;
-            ret->job_id = atoll(optarg);
+            result->cmd = CMD_REMOVE;
+            result->job_id = atoll(optarg);
             break;
 
         case 'i':
-            if (ret->cmd != -1) {
-                printf("%s", help);
+            if (result->cmd != -1) {
                 return -1;
             }
-            ret->cmd = CMD_INFO;
-            ret->job_id = atoll(optarg);
+            result->cmd = CMD_INFO;
+            result->job_id = atoll(optarg);
             break;
 
         case 'l':
-            if (ret->cmd != -1) {
-                printf("%s", help);
+            if (result->cmd != -1) {
                 return -1;
             }
-            ret->cmd = CMD_LIST;
-            ret->job_id = -1;
+            result->cmd = CMD_LIST;
+            result->job_id = -1;
             break;
 
+        case 'h':
+            fputs(DA_USAGE_HELP, stdout);
+            exit(EXIT_SUCCESS);
+
         case '?':
-            printf("%s", help);
             return -1;
             break;
         }
     }
 
-    if (optind < argc) {
-        printf("%s", help);
+    if (result->cmd == -1) {
+        // Missing command
         return -1;
     }
+
+    if (optind < argc) {
+        return -1;
+    }
+
     return 0;
 }
--- src/client/arg_parse.h
+++ src/client/arg_parse.h
@@ -1,18 +1,19 @@
 #ifndef ARG_PARSE_HEADER
 #define ARG_PARSE_HEADER
-#define DEFAULT_PRIORITY 2
 
-static char *help =
+#include <stdint.h>
+
+static const char DA_USAGE_HELP[] =
     "Usage: da [OPTION]... [DIR]...\n"
     "Analyze the space occupied by the directory at [DIR]\n"
-    "-a, --add analyze a new directory path for disk usage\n"
-    "-p, --priority set priority for the new analysis (works only with -a argument)\n"
+    "-a, --add          analyze a new directory path for disk usage\n"
+    "-p, --priority     set priority (1-3) for the new analysis (works only with -a argument)\n"
     "-S, --suspend <id> suspend task with <id>\n"
-    "-R, --resume <id> resume task with <id>\n"
-    "-r, --remove <id> remove the analysis with the given <id>\n"
-    "-i, --info <id> print status about the analysis with <id> (pending, progress, done)\n"
-    "-l, --list list all analysis tasks, with their ID and the corresponding root path\n"
-    "-p, --print <id> print analysis report for those tasks that are done\n";
+    "-R, --resume <id>  resume task with <id>\n"
+    "-r, --remove <id>  remove the analysis with the given <id>\n"
+    "-i, --info <id>    print status about the analysis with <id> (pending, progress, done)\n"
+    "-l, --list         list all analysis tasks, with their ID and the corresponding root path\n"
+    "-p, --print <id>   print analysis report for those tasks that are done\n";
 
 struct da_args {
     int8_t cmd;
@@ -24,6 +25,6 @@ struct da_args {
         };
     };
 };
-int get_args(int argc, char **argv, struct da_args* ret);
+int parse_args(int argc, char **argv, struct da_args* ret);
 
 #endif
