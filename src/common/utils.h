13eee37c3f21db5b6e182bfccb5b298626b988d3
Tezkerek <andrei.ancuta@gmail.com>
2023-01-03 19:31:27 +0200

    Send command to daemon from parsed args
    Refactor ipc.h to use a ByteArray struct to represent payloads

--- src/common/utils.h
+++ src/common/utils.h
@@ -2,6 +2,7 @@
 #define UTILS_HEADER
 
 #include <stddef.h>
+#include <stdint.h>
 
 int min(int a, int b);
 
@@ -11,5 +12,14 @@ int max(int a, int b);
  * malloc that exits on error
  */
 void *da_malloc(size_t size);
+
+struct ByteArray {
+    int64_t len;
+    char *bytes;
+};
+
+void bytearray_init(struct ByteArray *arr, int64_t len);
+
+void bytearray_destroy(struct ByteArray *arr);
 
 #endif
